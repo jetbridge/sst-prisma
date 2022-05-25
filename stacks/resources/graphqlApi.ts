19273558ff5e42fb52aee460c066318a269cedeb
Mischa Spiegelmock <me@mish.dev>
2022-05-24 17:47:43 -0700

    WIP

--- stacks/resources/graphqlApi.ts
+++ stacks/resources/graphqlApi.ts
@@ -2,7 +2,6 @@ import { AuthorizationType } from "@aws-cdk/aws-appsync-alpha";
 import { AppSyncApi, Stack } from "@serverless-stack/resources";
 import { Duration, Expiration } from "aws-cdk-lib";
 
-// presently unused - requires auth
 export class GraphqlApi extends AppSyncApi {
   constructor(scope: Stack, id: string) {
     super(scope, id, {
