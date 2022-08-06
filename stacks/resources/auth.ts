386c2f728837c6c3f06cbc9a25d902b225221093
Mischa Spiegelmock <me@mish.dev>
2022-08-05 20:27:03 -0700

    linkedin

--- stacks/resources/auth.ts
+++ stacks/resources/auth.ts
@@ -1,12 +1,23 @@
 import { Auth as SstAuth, StackContext } from '@serverless-stack/resources';
+import { StringAttribute } from 'aws-cdk-lib/aws-cognito';
 
 export function Auth({ stack }: StackContext) {
-  new SstAuth(stack, 'Auth', {
-    defaults: {
-      function: {},
+  const auth = new SstAuth(stack, 'Auth', {
+    identityPoolFederation: {
+      // plug in auth providers here
+      // https://docs.sst.dev/constructs/Auth#authcognitoidentitypoolfederationprops
     },
     triggers: {
+      // save user in DB
       preSignUp: 'backend/src/auth/trigger/preSignUp.handler',
+    },
+    cdk: {
+      userPool: {
+        customAttributes: {
+          first_name_orig: new StringAttribute({ mutable: true }),
+          last_name_orig: new StringAttribute({ mutable: true }),
+        },
+      },
     },
   });
 }
