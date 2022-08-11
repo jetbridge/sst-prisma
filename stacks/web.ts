d5112be27c7cab67e9074b6604849455067f0964
Mischa Spiegelmock <me@mish.dev>
2022-08-10 18:45:04 -0700

    WIP

--- stacks/web.ts
+++ stacks/web.ts
@@ -1,15 +1,18 @@
 import { StackContext, NextjsSite, use } from '@serverless-stack/resources';
 import { AppSyncApi } from './appSyncApi';
 import { Auth } from './auth';
+import { Dns } from './dns';
 
 export function Web({ stack }: StackContext) {
   const { userPool, clientId } = use(Auth);
   const appSyncApi = use(AppSyncApi);
+  const dns = use(Dns);
 
   // Web
   // docs: https://docs.serverless-stack.com/constructs/NextjsSite
   const frontendSite = new NextjsSite(stack, 'Web', {
     path: 'web',
+    customDomain: dns.domainName,
     environment: {
       NEXT_PUBLIC_REGION: stack.region,
       NEXT_PUBLIC_APPSYNC_ENDPOINT: appSyncApi.api.url,
