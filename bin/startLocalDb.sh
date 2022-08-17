2186076df2378f36cd828ef4eb5ec8722f753000
Mischa Spiegelmock <me@mish.dev>
2022-08-17 15:19:59 -0700

    local test env

--- bin/startLocalDb.sh
+++ bin/startLocalDb.sh
@@ -7,9 +7,7 @@ if [[ -n "${CI}" ]]; then
 	echo "DB is started by CI"
 else
 	echo "Starting DB"
-	docker-compose  --project-directory "${SCRIPT_DIR}/.." up -d
+	docker-compose  --project-directory "${SCRIPT_DIR}/.." up -d --remove-orphans
 	"$SCRIPT_DIR/waitForPg.sh"
-	docker-compose exec -T sst-local-db dropdb -U postgres --if-exists tests
-	docker-compose exec -T sst-local-db createdb -U postgres tests
 fi
 
--- testSetup.ts
+++ testSetup.ts
@@ -1,1 +1,14 @@
+// if running in CI (github actions) we should use the configured DB
 
+import dotenv from 'dotenv';
+import path from 'path';
+import { dirname } from 'desm';
+
+const __dirname = dirname(import.meta.url);
+
+if (!process.env.DATABASE_URL && !process.env.CI) {
+  dotenv.config({
+    path: path.join(__dirname, 'backend', '.env.vitest'),
+    debug: true,
+  });
+}
