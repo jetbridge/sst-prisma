a5c398a42055d9098ad488b3e367a910600306b6
Mischa Spiegelmock <me@mish.dev>
2022-08-16 21:31:13 -0700

    WIP

--- bin/startLocalDb.sh
+++ bin/startLocalDb.sh
@@ -3,19 +3,13 @@
 set -exo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-function deploy {
-	pnpm run deploy
-}
-
 if [[ -n "${CI}" ]]; then
 	echo "Running migrations in CI"
-	deploy
 else
 	echo "Starting DB and running migrations"
 	docker-compose  --project-directory "${SCRIPT_DIR}/.." up --force-recreate -d
 	"$SCRIPT_DIR/waitForPg.sh"
 	docker-compose exec -T sst-local-db dropdb -U postgres --if-exists tests
 	docker-compose exec -T sst-local-db createdb -U postgres tests
-	deploy
 fi
 
