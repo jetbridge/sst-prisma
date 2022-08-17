6d92129697e13b20f977ee93cce7567bd5bbc419
Mischa Spiegelmock <me@mish.dev>
2022-08-17 14:29:10 -0700

    fix db init

--- bin/startLocalDb.sh
+++ bin/startLocalDb.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [[ -n "${CI}" ]]; then
 	echo "Running migrations in CI"
 else
-	echo "Starting DB and running migrations"
-	docker-compose  --project-directory "${SCRIPT_DIR}/.." up --force-recreate -d
+	echo "Starting DB"
+	docker-compose  --project-directory "${SCRIPT_DIR}/.." up -d
 	"$SCRIPT_DIR/waitForPg.sh"
 	docker-compose exec -T sst-local-db dropdb -U postgres --if-exists tests
 	docker-compose exec -T sst-local-db createdb -U postgres tests
