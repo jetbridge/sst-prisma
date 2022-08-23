8fabf758d6d4ac7ae2554ce19f73129f4ebab0ff
Mischa Spiegelmock <me@mish.dev>
2022-08-22 18:58:52 -0700

    NEXTAUTH_SECRET

--- stacks/web.ts
+++ stacks/web.ts
@@ -2,11 +2,13 @@ import { NextjsSite, StackContext, use } from '@serverless-stack/resources';
 import { AppSyncApi } from './appSyncApi';
 import { Auth } from './auth';
 import { Dns } from './dns';
+import { Secrets } from './secrets';
 
 export function Web({ stack }: StackContext) {
   const { userPool, webClient, cognitoDomainName } = use(Auth);
   const appSyncApi = use(AppSyncApi);
   const dns = use(Dns);
+  const secrets = use(Secrets);
 
   // docs: https://docs.serverless-stack.com/constructs/NextjsSite
   const frontendSite = new NextjsSite(stack, 'Web', {
@@ -18,7 +20,8 @@ export function Web({ stack }: StackContext) {
         }
       : undefined,
     environment: {
-      NEXTAUTH_URL: 'http://localhost:6020', // FIXME: how to pass in this URL?
+      // NEXTAUTH_URL: 'http://localhost:6020', // FIXME: how to pass in this URL?
+      NEXTAUTH_SECRET: secrets.secret.secretValueFromJson('RANDOM').toString(), // FIXME https://github.com/nextauthjs/next-auth/discussions/5145 & https://github.com/serverless-stack/sst/issues/1986
       NEXT_PUBLIC_REGION: stack.region,
       NEXT_PUBLIC_APPSYNC_ENDPOINT: appSyncApi.api.url,
       NEXT_PUBLIC_COGNITO_CLIENT_ID: webClient.userPoolClientId,
