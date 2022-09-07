1dae15636e9d7c0285b71a77d0c4f8dab2c0a09c
Mischa Spiegelmock <me@mish.dev>
2022-09-06 19:16:11 -0700

    Common package reference, next.config.js support

---
+++ web/next.config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // minify faster
+  swcMinify: true,
+  experimental: {
+    // we don't need to import EVERY component or icon
+    // this rewrites imports to only import what we need from MUI
+    modularizeImports: {
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+    },
+  },
+};
+
+module.exports = nextConfig;
