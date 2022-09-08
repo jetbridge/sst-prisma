0d1ebc4754e8891032ed719f4a33762da15c361f
Héctor J. Vásquez <imvasen@gmail.com>
2022-09-07 23:11:43 -0500

    remove truncateAll from testUtil.ts

--- backend/src/db/testUtil.ts
+++ backend/src/db/testUtil.ts
@@ -1,20 +1,6 @@
 import { PrismaTestingHelper } from '@chax-at/transactional-prisma-testing';
+import { truncateAllTables } from '@backend/db/seed/truncate';
 import { PrismaClient } from '@prisma/client';
-import { isProd } from 'common';
-
-export async function truncateAllTables(prisma: PrismaClient) {
-  if (isProd()) throw new Error('Please stop whatever you are doing right now.');
-
-  await prisma.$executeRawUnsafe(`
-      DO $$ DECLARE
-          r RECORD;
-      BEGIN
-          FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema() AND tableowner = user) LOOP
-              EXECUTE 'TRUNCATE TABLE  ' || quote_ident(r.tablename) || ' CASCADE';
-          END LOOP;
-      END $$;
-      `);
-}
 
 const prismaClient = new PrismaClient();
 await truncateAllTables(prismaClient); // can be skipped for speed maybe
