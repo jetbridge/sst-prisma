97614f4461cea198534c16cc8563256211bb443c
Mischa Spiegelmock <me@mish.dev>
2022-09-08 19:12:07 -0700

    WIP

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const outputFileTracingRoot = path.join(__dirname, '../');
+console.log({ outputFileTracingRoot });
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -5,6 +11,8 @@ const nextConfig = {
   /* config options here */
 
   swcMinify: true,
+
+  // outputFileTracingRoot,
 };
 
 export default nextConfig;
