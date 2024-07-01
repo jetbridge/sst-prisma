32f144f6dffc104bfb75eaefb72003eae9ee1ef1
Roman Kyslyy <rkyslyy@gmail.com>
2024-07-01 16:49:20 +0300

    WIP

---
+++ web/auth.config.ts
@@ -0,0 +1,28 @@
+import type { NextAuthConfig } from 'next-auth';
+import { OAuthUserConfig } from 'next-auth/providers';
+import CognitoProvider, { CognitoProfile } from 'next-auth/providers/cognito';
+
+export const authConfig = {
+  providers: [
+    CognitoProvider({
+      clientId: userPoolWebClientId!,
+      issuer: `https://cognito-idp.${region}.amazonaws.com/${userPoolId}`,
+      idToken: true,
+
+      // use cognito for token signing
+      // https://github.com/nextauthjs/next-auth/issues/4707
+      clientSecret: '',
+      client: {
+        token_endpoint_auth_method: 'none',
+      },
+      // checks: "pkce", // https://github.com/nextauthjs/next-auth/discussions/3551
+      checks: ['nonce', 'pkce', 'state'], // https://github.com/nextauthjs/next-auth/discussions/3551
+      // checks: "nonce", // https://github.com/nextauthjs/next-auth/discussions/3551
+      authorization: { params: { identity_provider: 'Linkedin' } }, // skip cognito hosted UI
+      id: 'cognito',
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+} satisfies NextAuthConfig;
