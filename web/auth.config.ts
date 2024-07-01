e492aeda47355a8e28b48a53b806a686f1695e24
Roman Kyslyy <rkyslyy@gmail.com>
2024-07-01 16:52:27 +0300

    WIP

--- web/auth.config.ts
+++ web/auth.config.ts
@@ -1,12 +1,13 @@
 import type { NextAuthConfig } from 'next-auth';
 import { OAuthUserConfig } from 'next-auth/providers';
 import CognitoProvider, { CognitoProfile } from 'next-auth/providers/cognito';
+import { REGION } from './config';
 
 export const authConfig = {
   providers: [
     CognitoProvider({
       clientId: userPoolWebClientId!,
-      issuer: `https://cognito-idp.${region}.amazonaws.com/${userPoolId}`,
+      issuer: `https://cognito-idp.${REGION}.amazonaws.com/${userPoolId}`,
       idToken: true,
 
       // use cognito for token signing
