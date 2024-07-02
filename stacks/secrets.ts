df32cc02a270a3c943b3a1bead49396f2eac7e39
Mischa Spiegelmock <me@mish.dev>
2024-07-02 11:48:17 -0700

    secret naming

--- stacks/secrets.ts
+++ stacks/secrets.ts
@@ -3,7 +3,8 @@ import { Config, StackContext, use } from 'sst/constructs'
 import { Iam } from './iam'
 import { SECRETS_ARN } from './config'
 
-export function Secrets({ stack }: StackContext) {
+export function Secrets({ stack, app }: StackContext) {
+  // import existing secrets?
   const secretsArn = SECRETS_ARN
 
   // needed for NEXTAUTH_SECRET env var since there is no way to provide it via SST Config
@@ -15,6 +16,7 @@ export function Secrets({ stack }: StackContext) {
     secrets = secretsArn
       ? Secret.fromSecretCompleteArn(stack, 'Secrets', secretsArn)
       : new Secret(stack, 'App', {
+          secretName: app.logicalPrefixedName('app'),
           description: `${stack.stackName} ${stack.stage} secrets`,
           // secret default template
           generateSecretString: {
@@ -26,7 +28,11 @@ export function Secrets({ stack }: StackContext) {
   }
 
   // add more SST secrets here
+  // see SST Config docs for more info
   const SECRET_1 = new Config.Secret(stack, 'SECRET_1')
+
+  // grant your app permissions to access the SST secrets
+  app.addDefaultFunctionBinding([SECRET_1])
 
   return { secrets, SECRET_1 }
 }
