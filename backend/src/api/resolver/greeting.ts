22535e879afa07e53ee88ffb30d519788d6c011d
Mischa Spiegelmock <me@mish.dev>
2024-07-01 19:21:14 -0700

    builds

--- backend/src/api/resolver/greeting.ts
+++ backend/src/api/resolver/greeting.ts
@@ -1,25 +1,25 @@
-import { defaultAppSyncMiddleware } from '@backend/middleware/lambda';
-import { logger } from '@backend/util/logger';
-import { incrementMetric } from '@backend/util/metrics';
-import { AppSyncResolverEvent } from 'aws-lambda';
-import { GreetingResponse, GreetingState, MutationGreetArgs } from 'common/generated/graphql/graphql';
+import { defaultAppSyncMiddleware } from '@backend/middleware/lambda'
+import { logger } from '@backend/util/logger'
+import { incrementMetric } from '@backend/util/metrics'
+import { AppSyncResolverEvent } from 'aws-lambda'
+import { GreetingResponse, GreetingState, MutationGreetArgs } from '@common/generated/graphql/graphql'
 
-export const GREETING = 'Yo yo';
+export const GREETING = 'Yo yo'
 
 // sample query
 export const getGreeting = (): GreetingState => ({
   currentGreeting: GREETING,
-});
+})
 
 // sample mutation
 export const greetInner = async ({
   arguments: { name },
 }: AppSyncResolverEvent<MutationGreetArgs>): Promise<GreetingResponse> => {
-  incrementMetric('SaidHello');
-  logger.debug('Saying greeting to', { name });
+  incrementMetric('SaidHello')
+  logger.debug('Saying greeting to', { name })
 
   return {
     greeting: `${GREETING}, ${name}!`,
-  };
-};
-export const greet = defaultAppSyncMiddleware()(greetInner);
+  }
+}
+export const greet = defaultAppSyncMiddleware()(greetInner)
