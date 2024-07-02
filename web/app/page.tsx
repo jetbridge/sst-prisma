1f26f2f71f84579416734ac8fb89034dc3b42435
Roman Kyslyy <rkyslyy@gmail.com>
2024-07-02 16:39:33 +0300

    Clean up

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,6 @@
 import Home from '@/lib/component/home/Home'
-import { PageProps } from './types'
 
-export default function Page({ searchParams = {} }: PageProps<{ callbackUrl?: string }>) {
-  const { callbackUrl } = searchParams
-
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Home />
